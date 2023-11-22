a71897a46f32e60ad1ea5c61c648b73aa08d88a4
Thiago Lemos <talemos@users.noreply.github.com>
2023-11-22 16:22:04 -0300

    Create variables.tf
    Init Variables

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable tenancy_ocid {
+    type = string
+    default = "ocid1.tenancy.oc1..aaaaaaaa42xyl6t4mxwlraallr3sjmybpvn2h4fad2o7w4civqzp6hxniucq"
+}
+
+#OCI Compartment
+variable oci_compartment_id {
+    type = string
+    default = "ocid1.compartment.oc1..aaaaaaaar5ttrtgsceazeg327yi4junf5aox32k37bsq5ucxlkibz6tgbkra"
+} 
