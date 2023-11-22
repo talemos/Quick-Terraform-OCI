ce4d2d30d535e280c6567ce42d652ed7815f5b24
Thiago Lemos <talemos@users.noreply.github.com>
2023-11-22 16:19:32 -0300

    Create provider.tf
    Initial provider OCI

---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "oci" {
+	tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaa42xyl6t4mxwlraallr3sjmybpvn2h4fad2o7w4civqzp6hxniucq"
+	user_ocid = "ocid1.user.oc1..aaaaaaaa3s6v5bfqfklm4435w2hxgrakwrahp7kjtduvbrr2gxjovi7bwzxa" 
+	private_key_path = "~/.oci/oci_api_key.pem"
+	fingerprint = "33:93:45:e5:ca:82:df:ff:ec:88:97:de:44:89:41:5c"
+	region = "sa-saopaulo-1"
+}
